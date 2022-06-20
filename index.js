95c110a2107e1910e174b7ee0cc5e7f5b668408a
Benjamin Klarer <benjaminklarer@gmail.com>
2022-06-19 22:52:12 -0700

    commit tree 7fb6dd8e0baa01b889368f775e2bfb2c0b285fd4

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i=0; i<array.length - 1; i++) {
+    for(let j=i+1; j<array.length; j++) {
+      total = array[i] + array[j]
+      console.log(`i = ${array[i]} j = ${array[j]} TOTAL ${total} needs to = ${target}}`)
+      if(array[i]+array[j] === target) {
+        return true
+      } 
+    } 
+  } return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  need to add 2 items in an array and return true when they add up to the target
 */
 
 /*
   Add written explanation of your solution here
+  iterated over each item in the array to add 2 positions and check to see if the target equals the 2 positions added
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  hasTargetSum([3, 8, 12, 4, 11, 7], 10)
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
